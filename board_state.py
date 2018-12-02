607653aabbb60d088a6f238c38552e8ec029c8fd
Sasha Brooks <sasha.brooks@student.manchester.ac.uk>
2018-12-02 16:37:19 +0000

    MinMax

---
+++ board_state.py
@@ -0,0 +1,129 @@
+from bot_functions import get_legal_moves
+from bot_constants import *
+import sys
+
+log = open("log.txt", "w")
+
+class Hole:
+    def __init__(self, number, board_side):
+        self.number = number
+        self.board_side = board_side
+
+
+class BoardState:
+    def __init__(self):
+        self.north_board_score = 0
+        self.south_board_score = 0
+        self.north_board_state = {1:7, 2:7, 3:7, 4:7, 5:7, 6:7, 7:7}
+        self.south_board_state = {1:7, 2:7, 3:7, 4:7, 5:7, 6:7, 7:7}
+
+    def copy(self):
+        new_board_state = BoardState()
+        new_board_state.north_board_score = self.north_board_score
+        new_board_state.south_board_score = self.south_board_score
+        new_board_state.north_board_state = dict(self.north_board_state)
+        new_board_state.south_board_state = dict(self.south_board_state)
+
+        return new_board_state
+        
+        
+    def total_stones_on_side(self, board_side):
+        total = 0
+    
+        for key, val in self.get_board_state_for_side(board_side).items():
+            total += val
+            
+        return val
+    
+    
+    def do_we_play_again(self, move, board_side):
+        """ Uses a function xmod15 = 7 - n + 1 to decide if we get a second move after. 
+        x is the number of seeds in the chosen hole, n is the hole number
+        15 chosen because all the playing holes - the scoring well for opponent which
+        we don't use.
+        """
+        seeds_in_hole = self.get_board_state_for_side(board_side)[move] 
+        
+        return seeds_in_hole % 15 == 7 - move + 1
+    
+    
+    def get_score_for_side(self, board_side):
+        if board_side == BOARD_SIDE_NORTH:
+            return self.north_board_score
+        return self.south_board_score
+        
+        
+    def get_board_state_for_side(self, board_side):
+        if board_side == BOARD_SIDE_NORTH:
+            return self.north_board_state
+        return self.south_board_state
+            
+    def _increase_score_for_side(self, board_side, amt=1):
+        if board_side == BOARD_SIDE_NORTH:
+            self.north_board_score += amt
+        else:
+            self.south_board_score += amt
+
+    def get_next_state(self, hole_choice, player_board_side):
+        next_state = self.copy()
+
+        if hole_choice in get_legal_moves(self, player_board_side):
+            side_state = next_state.get_board_state_for_side(player_board_side)
+
+            remaining_seeds = side_state[hole_choice]
+            # Empty the selected hole
+            side_state[hole_choice] = 0
+
+            # Picked any hole but the last one before the scoring well
+            if hole_choice < 7:
+                next_hole = Hole(hole_choice + 1, player_board_side)
+            else:
+                # We chose the /final/ hole on the players side.
+                # Next hole is past the scoring well, so we switch sides and
+                # decrement the remaining seeds (1 goes to scoring well)
+                next_hole = Hole(1, not player_board_side)
+                remaining_seeds -= 1
+                next_state._increase_score_for_side(player_board_side)
+
+            while remaining_seeds > 0:
+                # Get state for the correct side
+                next_hole_board_state = next_state.get_board_state_for_side(next_hole.board_side)
+                # Increment the number of seeds in the next hole
+                remaining_seeds -= 1
+                
+                if remaining_seeds == 0  and next_hole.board_side == player_board_side:
+                    opp_next_hole_board_state = next_state.get_board_state_for_side(not next_hole.board_side)
+                    opp_hole_number = 7 - next_hole.number + 1
+                    
+                    captured_stones = opp_next_hole_board_state[opp_hole_number]
+                    next_state._increase_score_for_side(player_board_side, 1 + captured_stones)
+                    
+                    opp_next_hole_board_state[opp_hole_number] = 0
+                else:
+                    next_hole_board_state[next_hole.number] += 1
+         
+
+                # Now calculate the next hole
+                previous_hole = next_hole
+                if remaining_seeds != 0:
+                    next_hole_number = previous_hole.number + 1
+                    next_hole_side = previous_hole.board_side
+
+                    # Check if need to change sides
+                    if next_hole_number > 7:
+                        next_hole_number = 1
+                        next_hole_side = not next_hole_side
+
+                        # If we left the players side
+                        if previous_hole.board_side == player_board_side:
+                            next_state._increase_score_for_side(player_board_side)
+                            remaining_seeds -= 1
+
+                    next_hole = Hole(next_hole_number, next_hole_side)
+        else:
+            log.write("Action is not a legal move: " + str(hole_choice) + "\n")
+            sys.stderr.write("CHECK LOGS!  EXITING")
+
+            sys.exit(0)
+
+        return next_state
---
+++ bot.py
@@ -0,0 +1,234 @@
+#!/bin/python
+
+import sys
+import random
+from enum import Enum
+from board_state import BoardState
+from bot_functions import get_legal_moves
+from bot_constants import *
+
+# 1 is North, 0 is South
+board_side = 0
+
+epsilon = 0.05
+
+log = open("log.txt", "w")
+
+board_state = BoardState()
+
+def get_board_action_hash(state, action, player_board_side):
+    feature = 0
+    for i in state.south_board_state.keys():
+        feature = 31 * feature + state.south_board_state[i]
+
+    feature = 31 * feature + state.south_board_score
+
+    for i in state.north_board_state.keys():
+        feature = 31 * feature + state.north_board_state[i]
+
+    feature = 31 * feature + state.north_board_score
+    feature = 31 * feature + action
+    feature = 31 * feature + board_side
+
+    return feature
+
+def get_feature_vector(state, action, player_board_side):
+    features = []
+
+    state_after_action = state.get_next_state(action, player_board_side)
+
+    feature1 = get_board_action_hash(state, action, player_board_side)
+
+    features.append(feature1)
+
+    return features
+
+def decision(prob):
+    return random.random() < prob
+
+def dot(K, L):
+   if len(K) != len(L):
+      return 0
+
+   return sum(i[0] * i[1] for i in zip(K, L))
+
+epsilon = 0.3
+weight_vector = []
+made_a_move_yet = False
+
+
+
+def get_heuristic_val(board_state, max_player_side):
+    max_score = board_state.get_score_for_side(max_player_side)
+    min_score = board_state.get_score_for_side(not max_player_side)
+    
+    max_stones = board_state.total_stones_on_side(max_player_side)
+    min_stones = board_state.total_stones_on_side(not max_player_side)
+    
+    return (max_score - min_score) + (max_stones - min_stones)
+
+def get_max_value(board_state, board_side, depth):
+    curr_highest_val = 0
+    
+    possible_moves = get_legal_moves(board_state, board_side)
+    
+    if len(possible_moves) == 0:
+        stones_opp_side = board_state.total_stones_on_side(not board_side)
+        
+        board_state_copy = board_state.copy()
+        
+        board_state_copy._increase_score_for_side(not board_side, stones_opp_side)
+        
+        if board_state_copy.get_score_for_side(board_side) > board_state_copy.get_score_for_side(not board_side):
+            return float("inf")
+        elif board_state_copy.get_score_for_side(board_side) < board_state_copy.get_score_for_side(not board_side):
+            return float("-inf")
+        else:
+            return 0
+    
+    for move in possible_moves:
+        next_state = board_state.get_next_state(move, board_state)
+            
+        if depth > DEPTH_LIMIT:
+            val = get_heuristic_val(next_state, board_side)
+        else:
+            if board_state.do_we_play_again(move, board_side):
+                val = get_max_value(next_state, board_side, depth + 1)
+            else:
+                val = get_min_value(next_state, not board_side, depth + 1)
+            
+        if val > curr_highest_val:
+            curr_highest_val = val
+            
+    
+    return curr_highest_val
+
+def get_min_value(board_state, board_side, depth):
+    curr_lowest_val = 0
+    
+    possible_moves = get_legal_moves(board_state, board_side)
+    
+    if len(possible_moves) == 0:
+        stones_opp_side = board_state.total_stones_on_side(not board_side)
+        
+        board_state_copy = board_state.copy()
+        
+        board_state_copy._increase_score_for_side(not board_side, stones_opp_side)
+        
+        if board_state_copy.get_score_for_side(board_side) > board_state_copy.get_score_for_side(not board_side):
+            return float("-inf")
+        elif board_state_copy.get_score_for_side(board_side) < board_state_copy.get_score_for_side(not board_side):
+            return float("inf")
+        else:
+            return 0
+    
+    for move in possible_moves:
+        next_state = board_state.get_next_state(move, board_side)
+        
+        if depth > DEPTH_LIMIT:
+            val = get_heuristic_val(next_state, not board_side)
+        else:
+            if board_state.do_we_play_again(move, board_side):
+                val = get_min_value(next_state, board_side, depth + 1)
+            else:
+                val = get_max_value(next_state, not board_side, depth + 1)
+                
+        if val < curr_lowest_val:
+            curr_lowest_val = val
+            
+    
+    return curr_lowest_val
+
+def make_move():
+    possible_moves = get_legal_moves(board_state, board_side)
+
+    curr_highest_val = 0
+    best_move = possible_moves[0]
+
+    for move in possible_moves:
+        next_state = board_state.get_next_state(move, board_side)
+        
+        if board_state.do_we_play_again(move, board_side):
+            value = get_max_value(next_state, board_side, 1)
+        else:
+            value = get_min_value(next_state, not board_side, 1)
+        
+        if value > curr_highest_val:
+            best_move = move
+        
+    choice = best_move
+
+    move = "MOVE;" + str(choice) + "\n"
+
+    log.write(move)
+
+    sys.stdout.write(move)
+    sys.stdout.flush()
+
+    global made_a_move_yet
+    made_a_move_yet = True
+
+def on_start(*args):
+    """
+    Makes a move if SOUTH, otherwise goes back to listening
+    """
+    global board_side
+    if args[0] == 'North':
+        board_side = BOARD_SIDE_NORTH
+    else:
+        board_side = BOARD_SIDE_SOUTH
+        make_move()
+
+def on_change(*args):
+    move_swap, state, turn = args[0].split(';', 2)
+
+    # Check if <MOVE> or "SWAP"
+    if move_swap == 'SWAP':
+        global board_side
+        board_side = not board_side
+
+    global current_board_state
+    board_state_input = state.split(',')
+
+    for i in range(0,7):
+        board_state.north_board_state[i + 1] = int(board_state_input[i])
+
+    board_state.north_board_score = board_state_input[NUM_HOLES_PER_SIDE]
+
+    # Input is modular, we need to shift to the next 'part' which starts at
+    # index NUM_HOLES_PER_SIDE + 1
+    for i in range(NUM_HOLES_PER_SIDE + 1, 2 * NUM_HOLES_PER_SIDE + 1):
+        board_state.south_board_state[i - NUM_HOLES_PER_SIDE] = board_state_input[i]
+
+    board_state.south_board_score = board_state_input[2 * NUM_HOLES_PER_SIDE + 1]
+
+    if turn == 'YOU':
+        make_move()
+
+    # Ignore OPP turn.
+
+def on_end(*args):
+    sys.exit()
+
+message_options = {
+    'START': on_start,
+    'CHANGE': on_change,
+    'END': on_end,
+}
+
+
+def read_message(input):
+    """
+    Parses message on each input from the game engine and calls the func.
+    """
+    if input == 'END':
+        on_end()
+
+    message_name, message_arguments = input.split(';', 1)
+
+    message_options[message_name](message_arguments)
+
+
+if __name__ == "__main__":
+    while True:
+        read_message(input())

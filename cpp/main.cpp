6a9c6c66c8af87d99ee3344fba912dde2dbfb94f
Sasha Brooks <sasha.brooks@student.manchester.ac.uk>
2018-12-11 18:17:32 +0000

    asdsa

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -2,7 +2,44 @@
 #include "bot.h"
 
 
-template <typename H1, typename H2>
+using namespace std;
+
+array<int, 2> weights1 = {1, 1};
+array<int, 2> weights2 = {1, 1};
+
+int heuristic1(const Board& b, Side maxPlayerSide) {
+    int maxScore = b.getSeedsInStore(maxPlayerSide);
+    int minScore = b.getSeedsInStore(opposideSide(maxPlayerSide));
+
+    int maxSeeds = getSeedsOnBoardSide(b, maxPlayerSide);
+    int minSeeds = getSeedsOnBoardSide(b, opposideSide(maxPlayerSide));
+
+    int stonesInFirstTwo = b.getSeeds(maxPlayerSide, 1) + b.getSeeds(maxPlayerSide, 2);
+    int stonesInLastTwo = b.getSeeds(maxPlayerSide, 6) + b.getSeeds(maxPlayerSide, 7);
+
+    int value = weights1[0] * (maxScore - minScore) + weights1[1] * (maxSeeds - minSeeds);
+
+    return value;
+}
+
+int heuristic2(const Board& b, Side maxPlayerSide) {
+    int maxScore = b.getSeedsInStore(maxPlayerSide);
+    int minScore = b.getSeedsInStore(opposideSide(maxPlayerSide));
+
+    int maxSeeds = getSeedsOnBoardSide(b, maxPlayerSide);
+    int minSeeds = getSeedsOnBoardSide(b, opposideSide(maxPlayerSide));
+
+    int stonesInFirstTwo = b.getSeeds(maxPlayerSide, 1) + b.getSeeds(maxPlayerSide, 2);
+    int stonesInLastTwo = b.getSeeds(maxPlayerSide, 6) + b.getSeeds(maxPlayerSide, 7);
+
+    int value = weights2[0] * (maxScore - minScore) + weights2[1] * (maxSeeds - minSeeds);
+
+    return value;
+}
+
+
+
+
 bool playGame(Bot& b, Bot& b2) {
     Board globalBoard;
 
@@ -49,22 +86,33 @@ bool playGame(Bot& b, Bot& b2) {
         int bScore = globalBoard.getSeedsInStore(bSide) + getSeedsOnBoardSide(globalBoard, bSide);
         int b2Score = globalBoard.getSeedsInStore(b2Side) + getSeedsOnBoardSide(globalBoard, b2Side);
 
-        bool winner = bScore > b2Score ? true : false;
-
-        return winner;
+        return bScore > b2Score;
     }
 }
 
 int main() {
+    Bot b1(heuristic1);
+    Bot b2(heuristic2);
 
-    try{
+    while (true) {
+        bool b1Wins = playGame(b1, b2);
 
-        Bot b(defaultHeuristic);
-      for (;;) {
-        b.runBot();
-      }
-    } catch(const std::system_error& e) {
-        std::cout << "Caught system_error with code " << e.code() 
-                  << " meaning " << e.what() << '\n';
+        if (b1Wins) {
+            weights2[0] = weights1[0] + rand() % 10 - 5;
+            weights2[1] = weights1[1] + rand() % 10 - 5;
+
+            std::cout << weights1[0] << ", " << weights1[1] << "\n";
+        }
+        else {
+            weights1[0] = weights2[0] + rand() % 10 - 5;
+            weights1[1] = weights2[1] + rand() % 10 - 5;
+
+            std::cout << weights2[0] << ", " << weights2[1] << "\n";
+        }
+
+
     }
+
+
+
 }
